70ed2df8628c6da9b657b5741e37699c9c2fad22
Jimin Kim <0305jimin@gmail.com>
2022-09-17 03:33:59 +0900

    Chore: adding initial project

---
+++ FindDict/FindDict/Global/Base/BaseVC.swift
@@ -0,0 +1,45 @@
+//
+//  BaseVC.swift
+//  FindDict
+//
+//  Created by 김지민 on 2022/09/17.
+//
+
+import UIKit
+
+class BaseVC: UIViewController, UIGestureRecognizerDelegate {
+    
+    // MARK: Properties
+    let screenWidth = UIScreen.main.bounds.size.width
+    let screenHeight = UIScreen.main.bounds.size.height
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setBackGroundColor()
+    }
+}
+
+// MARK: - Custom Methods
+extension BaseVC {
+    func hideTabbar() {
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    func showTabbar() {
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    /// 화면 터치시 키보드 내리는 메서드
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.delegate = self
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func setBackGroundColor() {
+        // TODO: - 컬러 넣기
+//        view.backgroundColor =
+    }
+}
