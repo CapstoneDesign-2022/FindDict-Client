bceb5d2ec9d060ee0bb9461a58258c6b4e12c41f
Jimin Kim <0305jimin@gmail.com>
2022-09-21 16:49:12 +0900

    Add files via upload

---
+++ FindDict/FindDict/Sources/Scences/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindDict
+//
+//  Created by 김지민 on 2022/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
