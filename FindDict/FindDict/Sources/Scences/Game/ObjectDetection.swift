59760ef56c5cf78e66e1351ca32247237c587257
Jimin Kim <0305jimin@gmail.com>
2022-10-05 19:57:10 +0900

    //WIP 버튼 클릭 안되는 오류 해결 시도#20

--- FindDict/FindDict/Sources/Scences/Game/ObjectDetection.swift
+++ FindDict/FindDict/Sources/Scences/Game/ObjectDetection.swift
@@ -16,15 +16,39 @@ class ObjectDetectionVC:ViewController {
     
     var pixelBuffer:CVPixelBuffer? = nil {
         didSet{
+//            print("pixel buffer set")
             setUpModel()
             handleImage(pixelBuffer: pixelBuffer)
         }
     }
     var image = UIImageView()
     
-    let objectDectectionModel = yolov7()
-    //    yolov5m()
+    let objectDectectionModel = yolov5m()
+    //    yolov7()
+    //yolov5m()
     var predictions: [VNRecognizedObjectObservation] = []
+    var label: String? {
+        return predictions.first?.labels.first?.identifier
+    }
+    lazy var buttons: [UIButton] = [] {
+        
+        didSet{
+//            print("buttons",buttons)
+            for button in buttons {
+                print(button.titleLabel?.text)
+//                print("buttonTitle",image.subviews)
+//                button.addTarget(nil,action: #selector(buttonClicked), for: .touchUpInside)
+                button.press{
+                    
+                    button.setImage(UIImage(named: "icon"), for: .normal)
+                }
+                image.addSubview(button)
+            }
+            
+            
+            
+        }
+    }
     
     // MARK: - Vision Properties
     var request: VNCoreMLRequest?
@@ -36,7 +60,21 @@ class ObjectDetectionVC:ViewController {
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+//        print("view did load")
         setLayout()
+//        print(image.subviews) //???: 빈 배열 나옴
+//        image.subviews.forEach({view in
+//                        type(of: view) == UIButton.self
+//                        if (view is UIButton) {
+//                            view.addTarget(self,action: #selector(buttonClicked), for: .touchUpInside)
+//                        }
+//            print(view)
+//            if let button = view as? UIButton {
+//                print(button.titleLabel?.text)
+//                button.addTarget(self,action: #selector(buttonClicked), for: .touchUpInside)
+//
+//            }
+//        }) //???: subviews가 빈 배열이라 수행 안 됨
     }
     static private var colors: [String: UIColor] = [:]
     
@@ -79,18 +117,11 @@ class ObjectDetectionVC:ViewController {
     // MARK: - Post-processing
     // 요청 실행 후
     func visionRequestDidComplete(request: VNRequest, error: Error?) {
-        
-
         if let predictions = request.results as? [VNRecognizedObjectObservation] {
-            print(predictions.first?.labels.first?.identifier ?? "nil")
-            print(predictions.first?.labels.first?.confidence ?? -1)
-            
             DispatchQueue.main.async {
                 //                self.image.layer.addSublayer(boxesView)
-//                self.boxesView.bounds = self.image.bounds
+                //                self.boxesView.bounds = self.image.bounds
                 //                self.boxesView.bounds = self.image.frame
-                
-//                self.boxesView.predictedObjects = predictions
                 self.predictedObjects = predictions
                 self.isInferencing = false
             }
@@ -102,64 +133,67 @@ class ObjectDetectionVC:ViewController {
     public var predictedObjects: [VNRecognizedObjectObservation] = [] {
         didSet {
             putButtons(with: predictedObjects)
-//            setNeedsDisplay()
+//            image.setNeedsDisplay()
         }
-    }
-    
-    var label: String? {
-        return predictions.first?.labels.first?.identifier
     }
     
     func putButtons(with predictions: [VNRecognizedObjectObservation]) {
-//        subviews.forEach({ $0.removeFromSuperview() })
-        
+        //        subviews.forEach({ $0.removeFromSuperview() })
+        var createdButtons:[UIButton]=[]
         for prediction in predictions {
-            createButton(prediction: prediction)
+            createdButtons.append(createButton(prediction: prediction))
+            
         }
+        buttons = createdButtons
     }
     
-    func createButton(prediction: VNRecognizedObjectObservation) {
+    func createButton(prediction: VNRecognizedObjectObservation)-> UIButton {
+//        print("create button")
         let buttonTitle: String? = prediction.label
         let color: UIColor = labelColor(with: buttonTitle ?? "N/A")
-//        let scale = CGAffineTransform.identity.scaledBy(x: image.bounds.width, y: image.bounds.height)
-//        print("scale",scale)
-//        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -1)
-        print(prediction.boundingBox.origin.x,prediction.boundingBox.origin.y, prediction.boundingBox.width,prediction.boundingBox.height)
-//        let buttonRect = prediction.boundingBox.applying(scale)
-//        print(buttonRect)
-//        print(labelString,bgRect)
-//        let buttonRect = CGRect(x: prediction.boundingBox.origin.x, y: prediction.boundingBox.origin.y, width: prediction.boundingBox.width, height: prediction.boundingBox.height)
-        
+        //        let scale = CGAffineTransform.identity.scaledBy(x: image.bounds.width, y: image.bounds.height)
+        //        print("scale",scale)
+        //        let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: -1)
+//        print(prediction.boundingBox.origin.x,prediction.boundingBox.origin.y, prediction.boundingBox.width,prediction.boundingBox.height)
+        //        let buttonRect = prediction.boundingBox.applying(scale)
+        //        print(buttonRect)
+        //        print(labelString,bgRect)
+        //        let buttonRect = CGRect(x: prediction.boundingBox.origin.x, y: prediction.boundingBox.origin.y, width: prediction.boundingBox.width, height: prediction.boundingBox.height)
+        //TODO: 스케일 맞추기
         let buttonRect = CGRect(x: prediction.boundingBox.origin.x*500, y: prediction.boundingBox.origin.y*500, width: prediction.boundingBox.width*500, height: prediction.boundingBox.height*500)
-        print(buttonRect)
-        let button = UIButton(frame: buttonRect)
+//        let button = UIButton(frame: buttonRect)
+        let button = UIButton(type: .custom)
+        button.frame = buttonRect
         button.layer.borderColor = color.cgColor
         button.backgroundColor = .systemBlue
         button.layer.borderWidth = 4
         button.backgroundColor = UIColor.clear
-//        button.titleLabel?.text = buttonTitle
-//        button.titleLabel?.font = .findDictH4M48
-//        button.titleLabel?.textColor = .black
         button.setTitle(buttonTitle, for: .normal)
-        image.addSubview(button)
-        button.press{
-            print(buttonTitle)
-        }
-//        let label = UILabel()
-//        label.text = buttonTitle ?? "N/A"
-//        label.font = UIFont.systemFont(ofSize: 13)
-//        label.textColor = UIColor.black
-//        label.sizeToFit()
-//        label.frame = CGRect(x: buttonRect.origin.x, y: buttonRect.origin.y - label.frame.height,
-//                             width: label.frame.width, height: label.frame.height)
-//        image.addSubview(label)
+        button.titleLabel?.alpha = 0
+        
+//        buttons.append(button)
+        
+//        button.addTarget(nil,action: #selector(buttonClicked), for: .touchUpInside)
+//        image.addSubview(button)
+//        print("buttonTitle",image.subviews)
+//                button.press{
+//                    print(buttonTitle)
+//                    button.setImage(UIImage(named: "icon"), for: .normal)
+//                }
+//        image.setNeedsDisplay()
+//        image.layoutIfNeeded()
+        return button
+    }
+    @objc func buttonClicked(_ sender:UIButton) -> Void {
+//        print(sender.titleLabel?.text)
+        print("Clicked")
     }
 }
 // MARK: - UI
 extension ObjectDetectionVC {
     func setLayout() {
         view.addSubViews([image
-//                          ,boxesView
+                          //                          ,boxesView
                          ])
         image.snp.makeConstraints{
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(17)
@@ -167,8 +201,8 @@ extension ObjectDetectionVC {
             $0.bottom.lessThanOrEqualTo(view.safeAreaLayoutGuide.snp.bottom).inset(50)
         }
         
-//        boxesView.snp.makeConstraints{
-//            $0.edges.equalTo(image)
-//        }
+        //        boxesView.snp.makeConstraints{
+        //            $0.edges.equalTo(image)
+        //        }
     }
 }
