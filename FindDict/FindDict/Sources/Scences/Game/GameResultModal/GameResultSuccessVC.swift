0afc4274845e39c5e082c232c17411c0265d7972
Jimin Kim <0305jimin@gmail.com>
2022-11-03 21:04:25 +0900

    Chore: 불필요한 모드 및 주석 삭제#34

--- FindDict/FindDict/Sources/Scences/Game/GameResultModal/GameResultSuccessVC.swift
+++ FindDict/FindDict/Sources/Scences/Game/GameResultModal/GameResultSuccessVC.swift
@@ -20,52 +20,5 @@ class GameResultSuccessVC: ModalBaseVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
-//        mainViewButton.press{
-//            //            guard let navigation = self.presentingViewController?.navigationController else { return }
-//            //            self.dismiss(animated: true, completion: {
-//            //                navigation.popToRootViewController(animated: true)
-//            //            })
-//            
-//            guard let pvc = self.presentingViewController else { return }
-////            self.dismiss(animated: true) {
-////                pvc.navigationController?.pushViewController(MainVC(), animated: true)
-////            }
-//            pvc.dismiss(animated: true) {
-//                pvc.navigationController?.popToRootViewController(animated: true)
-////                pvc.navigationController?.pushViewController(MainVC(), animated: true)
-//            }
-//        }
-//        setButtonActions()
     }
-    
-//    func setButtonActions(){
-//        retryButton.press{
-//            guard let pvc = self.presentingViewController else { return }
-//            self.dismiss(animated: true, completion: {
-//                pvc.navigationController?.pushViewController(PhotoSelectorVC(), animated: true)
-//            })
-////            self.view.window?.rootViewController?.dismiss(animated: false, completion: {
-////              let initiatingGameVC = PhotoSelectorVC()
-////                guard let presentingVC = self.presentingViewController as? UINavigationController else { return }
-////                self.view.window?.rootViewController?.dismiss(animated: false){
-////                           presentingVC.popToRootViewController(animated: true)}
-////            })
-//        }
-//
-//        dictionaryButton.press{
-//            guard let pvc = self.presentingViewController else { return }
-////            let dictionaryVC = DictionaryVC()
-//            self.dismiss(animated: true, completion: {
-//                pvc.navigationController?.pushViewController(DictionaryVC(), animated: true)
-//            })
-////            self.navigationController?.pushViewController(dictionaryVC, animated: true)
-//        }
-//
-//        mainViewButton.press{
-//            guard let pvc = self.presentingViewController else { return }
-//            self.dismiss(animated: true, completion: {
-//                pvc.navigationController?.pushViewController(MainVC(), animated: true)
-//            })
-//        }
-//    }
 }
