20a66fdca508593ced465c8cfdf1273c0b03e2eb
soeun <separk314@naver.com>
2022-11-04 20:38:01 +0900

    Chore: 게임방법 CVCModel 내용 수정#28

--- FindDict/FindDict/Sources/Scences/Main/GameTutorialCVCModel.swift
+++ FindDict/FindDict/Sources/Scences/Main/GameTutorialCVCModel.swift
@@ -19,11 +19,10 @@ struct GameTutorialCVCModel {
 // MARK: - Extensions
 extension GameTutorialCVCModel {
     static var sampleData: [GameTutorialCVCModel] = [
-        GameTutorialCVCModel(tutorialTitle: "숨은 그림 찾기", tutorialImageTitle: "tutorialExampleImage", tutorialText: "맞으면 좋아요~ 동그라미가 나오고 단어 리스트에 있는 단어들을 사진에서 찾아서 클릭해보세요~!! "),
-        GameTutorialCVCModel(tutorialTitle: "잘못 찾았다면?", tutorialImageTitle: "tutorialExampleImage", tutorialText: "틀리면 안 좋아요."),
-        GameTutorialCVCModel(tutorialTitle: "힌트 보는 법", tutorialImageTitle: "tutorialExampleImage", tutorialText: "모르겠으면 한글 단어를 클릭해 보세요. 힌트가 나온답니다,,,"),
-        GameTutorialCVCModel(tutorialTitle: "Game Over...", tutorialImageTitle: "tutorialExampleImage", tutorialText: "단어 리스트에 있는 단어를 모두 다 찾거나 3번 이상 틀리면 게임이 끝나요!!!"),
-        GameTutorialCVCModel(tutorialTitle: "단어장", tutorialImageTitle: "tutorialExampleImage", tutorialText: "단어장에 가면 이때까지 게임했던 단어들을 모아 볼 수 있어요"),
-        GameTutorialCVCModel(tutorialTitle: "단어장 사진 보기", tutorialImageTitle: "tutorialExampleImage", tutorialText: "사진 보기 버튼을 클릭하면 게임했던 사진도 볼 수 있답니다~~~~~"),
+        GameTutorialCVCModel(tutorialTitle: "숨은 그림 찾기 게임하기", tutorialImageTitle: "tutorialExampleImage", tutorialText: "그림 속에서 단어들을 찾아서 클릭해보세요!"),
+        GameTutorialCVCModel(tutorialTitle: "찾은 물체 발음 듣기", tutorialImageTitle: "tutorialExampleImage", tutorialText: "물체를 찾았을 때 물체의 영어 단어를 미국식/영국식/호주식 발음으로 들을 수 있어요"),
+        GameTutorialCVCModel(tutorialTitle: "힌트", tutorialImageTitle: "tutorialExampleImage", tutorialText: "모르는 단어일 때 단어를 클릭해보세요! 힌트를 볼 수 있어요"),
+        GameTutorialCVCModel(tutorialTitle: "Game Over...", tutorialImageTitle: "tutorialExampleImage", tutorialText: "3번 이상 틀리면 게임이 종료됩니다. 다시 게임을 할 수도 있고, 단어장에 가서 게임한 단어들을 다시 볼 수 있어요"),
+        GameTutorialCVCModel(tutorialTitle: "단어장", tutorialImageTitle: "tutorialExampleImage", tutorialText: "'사진 확인하기' 버튼을 클릭하면 게임한 사진을 다시 확인해볼 수 있어요"),
     ]
 }
