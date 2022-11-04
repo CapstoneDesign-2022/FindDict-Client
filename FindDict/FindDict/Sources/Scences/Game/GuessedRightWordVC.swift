228bcc0c26bd3d5cd56498f2b08d5af9740a0fdb
Jimin Kim <0305jimin@gmail.com>
2022-11-04 16:36:23 +0900

    Feat: 모달 완성#37

--- FindDict/FindDict/Sources/Scences/Game/GuessedRightWordVC.swift
+++ FindDict/FindDict/Sources/Scences/Game/GuessedRightWordVC.swift
@@ -12,8 +12,11 @@ import Then
 
 class GuessedRightWordVC: UIViewController {
     
-    
-    private let englishLabel = UILabel()
+    // MARK: - Properties
+    private let englishLabel = UILabel().then{
+        $0.font = .findDictH4R35
+        $0.textColor = .black
+    }
     private var englishText:String = ""{
         didSet{
             englishLabel.text = englishText
@@ -21,12 +24,25 @@ class GuessedRightWordVC: UIViewController {
         }
     }
     private let synthesizer = AVSpeechSynthesizer()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    private let modalView = UIView().then{
+        $0.backgroundColor = .bgBeige
+        $0.layer.shadowRadius = 4
+        $0.layer.shadowOffset = CGSize(width: 0, height: 4)
+        $0.layer.shadowColor = UIColor.black.cgColor
+        $0.layer.shadowOpacity = 0.25
     }
     
+    private let closeButton = UIButton().then{
+        $0.setImage(UIImage(named: "closeImage"), for: .normal)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .bgModal
+        setLayout()
+        setButtonActions()
+    }
     
     func setEnglishText(text:String){
         englishText = text
@@ -37,5 +53,36 @@ class GuessedRightWordVC: UIViewController {
         utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
         utterance.rate = 0.4
         synthesizer.speak(utterance)
+    }
+    
+    private func setButtonActions(){
+        closeButton.press{
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+}
+
+
+// MARK: - UI
+extension GuessedRightWordVC {
+    private func setLayout() {
+        view.addSubViews([modalView, englishLabel, closeButton])
+        
+        modalView.snp.makeConstraints{
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(236)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(223)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(223)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(92)
+        }
+        englishLabel.snp.makeConstraints{
+            $0.top.equalTo(modalView.snp.top).offset(40)
+            $0.centerX.equalTo(modalView)
+        }
+        closeButton.snp.makeConstraints{
+            $0.top.equalTo(modalView.snp.top).offset(16)
+            $0.trailing.equalTo(modalView.snp.trailing).inset(20)
+      
+        }
+        
     }
 }
