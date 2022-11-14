df4737e3dc891b284c6cbfb5e26da285aef0b7ee
soeun <separk314@cau.ac.kr>
2022-11-14 19:56:35 +0900

    Feat: API 변경에 따른 ResponseModel 수정#45

--- FindDict/FindDict/Sources/Scences/Dictionary/DictionaryVC.swift
+++ FindDict/FindDict/Sources/Scences/Dictionary/DictionaryVC.swift
@@ -12,7 +12,7 @@ import Then
 final class DictionaryVC: UIViewController {
     
     // MARK: - Properties
-    private var dictionaryData: [WordListResponseModel.Word]?
+    private var dictionaryData: [String]?
     private let titleView = UIView().then{
         $0.backgroundColor = .modalButtonDarkYellow
         $0.layer.shadowRadius = 4
@@ -75,7 +75,7 @@ extension DictionaryVC {
             switch networkResult {
             case .success(let response):
                 if let res = response as? WordListResponseModel {
-                    self.dictionaryData = res.words
+                    self.dictionaryData = res.english
                     self.dictionaryTV.reloadData()
                 }
                 
@@ -151,7 +151,7 @@ extension DictionaryVC: UITableViewDelegate {
 extension DictionaryVC: DictionaryCardDelegate {
     func wordDetailViewButtonClicked(index: Int) {
         let dictionaryDetailVC = DictionaryDetailVC()
-        dictionaryDetailVC.setWordLabelText(english: dictionaryData?[index].english ?? WordListResponseModel.sampleData[index].english)
+        dictionaryDetailVC.setWordLabelText(english: dictionaryData?[index] ?? WordListResponseModel.sampleData[index])
         dictionaryDetailVC.modalPresentationStyle = .overCurrentContext
         self.present(dictionaryDetailVC, animated: true)
     }
