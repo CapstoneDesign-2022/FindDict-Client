3c63de8c1b74ec0f5513c3b06e47f6c23c8d70a9
Jimin Kim <0305jimin@gmail.com>
2022-12-08 15:44:07 +0900

    Feat: 게임 종료 효과음 #92

--- FindDict/FindDict/Sources/Scences/Game/GameResultModal/GameResultSuccessVC.swift
+++ FindDict/FindDict/Sources/Scences/Game/GameResultModal/GameResultSuccessVC.swift
@@ -8,18 +8,34 @@
 import UIKit
 import Then
 import SnapKit
+import AVFoundation
 
 class GameResultSuccessVC: ModalBaseVC {
+    
+    // MARK: - Properties
+    var player: AVAudioPlayer?
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        playSound()
     }
     
     // MARK: - UI
     func setUI(){
         resultImage.image = UIImage(named: "successImage")
         resultTitleLabel.text = "Game Clear"
+    }
+    
+    func playSound(){
+        guard let url = Bundle.main.url(forResource: "GameClear", withExtension: "wav") else { return }
+            do {
+                player = try AVAudioPlayer(contentsOf: url)
+                guard let player = player else { return }
+                player.play()
+            } catch let error {
+                print(error.localizedDescription)
+            }
     }
 }
